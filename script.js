855a85c25e6ab0a76c4d6dcff1fbca0c1c9cf4b3
innab <innabraun08@gmail.com>
2021-02-16 18:48:58 +0100

    new 9hm

--- script.js
+++ script.js
@@ -4,33 +4,25 @@ document.addEventListener("DOMContentLoaded",()=>{
         tabsContent=document.querySelectorAll(".tab-content"),
         tabsParent=document.querySelector(".tabs");
 
-    function hideTabContent(){
-        tabsContent.forEach(item=>{
-            item.style.display="none";
-        });
-
-        tabs.forEach(item=>{
-            item.classList.remove("tabs-title.active")
-        });
-    }
-
-    function showTabContent(i=0){
-        tabsContent[i].style.display="block";
-        tabs[i].classList.add("tabs-title.active");
-    }
-    hideTabContent();
-    showTabContent();
 
 tabsParent.addEventListener("click",(event)=>{
     const target=event.target;
-    if(target && target.classList.contains("tabs-title")){
-        tabs.forEach((item,i)=>{
-            if (target==item){
-                hideTabContent();
-                showTabContent(i)
-            }
-        })
-    }
+    tabs.forEach((item,index)=>{
+        item.classList.remove("active")
+
+        tabsContent[index].classList[target.dataset.title === tabsContent[index].dataset.content ? 'add' : 'remove']('active');
+    })
+    target.classList.add("active");
+
+   /* tabsContent.forEach(item=>{
+        if(target.dataset.title===item.dataset.content){
+            item.classList.add("active")
+        }else {
+            item.classList.remove("active")
+        }
+    })*/
+
+
 })
 
 })
